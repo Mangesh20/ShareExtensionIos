23270cf648935f1ca586f19bc7370249c236340e
Mangesh <mangesh20@gmail.com>
2016-06-01 19:58:49 +0530

    url link

--- ExtensionObject/ShareViewController.swift
+++ ExtensionObject/ShareViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Maheshwari on 12/05/16.
 //  Copyright © 2016 Maheshwari. All rights reserved.
 //
-
+//https://developer.apple.com/library/ios/documentation/General/Conceptual/ExtensibilityPG/ExtensionScenarios.html
 import UIKit
 import MobileCoreServices
 import Social
@@ -16,6 +16,7 @@ class ShareViewController: SLComposeServiceViewController {
     @IBOutlet weak var infoTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+
         for item: AnyObject in (self.extensionContext?.inputItems)! {
             let inputItem = item as! NSExtensionItem
             for provider: AnyObject in inputItem.attachments! {
@@ -33,6 +34,7 @@ class ShareViewController: SLComposeServiceViewController {
 
     }
     
+
     override func didSelectPost() {
         // Perform the post operation.
         // When the operation is complete (probably asynchronously), the Share extension should notify the success or failure, as well as the items that were actually shared.
--- SafariExtensionDemo/ViewController.swift
+++ SafariExtensionDemo/ViewController.swift
@@ -15,32 +15,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let content = extensionContext!.inputItems[0] as! NSExtensionItem
-        let contentType = kUTTypeImage as String
-        
-        for attachment in content.attachments as! [NSItemProvider] {
-            if attachment.hasItemConformingToTypeIdentifier(contentType) {
-                
-                attachment.loadItemForTypeIdentifier(contentType, options: nil) { data, error in
-                    if error == nil {
-                        let url = data as! NSURL
-                        if NSData(contentsOfURL: url) != nil {
-//                            self.selectedImage = UIImage(data: imageData)
-                        }
-                    } else {
-                        
-                        let alert = UIAlertController(title: "Error", message: "Error loading image", preferredStyle: .Alert)
-                        
-                        let action = UIAlertAction(title: "Error", style: .Cancel) { _ in
-                            self.dismissViewControllerAnimated(true, completion: nil)
-                        }
-                        
-                        alert.addAction(action)
-                        self.presentViewController(alert, animated: true, completion: nil)
-                    }
-                }
-            }
-        }
     }
 
     override func didReceiveMemoryWarning() {
