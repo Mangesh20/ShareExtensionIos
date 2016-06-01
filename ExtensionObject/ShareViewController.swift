046b037c77062a79b34a0164736af7e20097e305
Mangesh <mangesh20@gmail.com>
2016-06-01 19:13:12 +0530

    1. intiial commit

---
+++ ExtensionObject/ShareViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ShareViewController.swift
+//  ExtensionObject
+//
+//  Created by Maheshwari on 12/05/16.
+//  Copyright © 2016 Maheshwari. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+import Social
+
+class ShareViewController: SLComposeServiceViewController {
+    
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var infoTextView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for item: AnyObject in (self.extensionContext?.inputItems)! {
+            let inputItem = item as! NSExtensionItem
+            for provider: AnyObject in inputItem.attachments! {
+                let itemProvider = provider as! NSItemProvider
+                
+                if itemProvider.hasItemConformingToTypeIdentifier(kUTTypePropertyList as String) {
+                    itemProvider.loadItemForTypeIdentifier(kUTTypePropertyList as String, options: nil, completionHandler: { (result: NSSecureCoding?, error: NSError!) -> Void in
+                        if let resultDict = result as? NSDictionary {
+                            print(resultDict)
+                        }
+                    })
+                }
+            }
+        }
+
+    }
+    
+    override func didSelectPost() {
+        // Perform the post operation.
+        // When the operation is complete (probably asynchronously), the Share extension should notify the success or failure, as well as the items that were actually shared.
+        super.didSelectPost()
+        
+        for item: AnyObject in (self.extensionContext?.inputItems)! {
+            let inputItem = item as! NSExtensionItem
+            
+            for provider: AnyObject in inputItem.attachments! {
+                let itemProvider = provider as! NSItemProvider
+                
+                if itemProvider.hasItemConformingToTypeIdentifier(kUTTypePropertyList as String) {
+                    itemProvider.loadItemForTypeIdentifier(kUTTypePropertyList as String, options: nil, completionHandler: { (result: NSSecureCoding?, error: NSError!) -> Void in
+                        if let resultDict = result as? NSDictionary {
+                            print(resultDict)
+                        }
+                    })
+                }
+            }
+        }
+
+    }
+    override func didSelectCancel() {
+        
+    }
+}
--- SafariExtensionDemo/ViewController.swift
+++ SafariExtensionDemo/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let content = extensionContext!.inputItems[0] as! NSExtensionItem
+        let contentType = kUTTypeImage as String
+        
+        for attachment in content.attachments as! [NSItemProvider] {
+            if attachment.hasItemConformingToTypeIdentifier(contentType) {
+                
+                attachment.loadItemForTypeIdentifier(contentType, options: nil) { data, error in
+                    if error == nil {
+                        let url = data as! NSURL
+                        if NSData(contentsOfURL: url) != nil {
+//                            self.selectedImage = UIImage(data: imageData)
+                        }
+                    } else {
+                        
+                        let alert = UIAlertController(title: "Error", message: "Error loading image", preferredStyle: .Alert)
+                        
+                        let action = UIAlertAction(title: "Error", style: .Cancel) { _ in
+                            self.dismissViewControllerAnimated(true, completion: nil)
+                        }
+                        
+                        alert.addAction(action)
+                        self.presentViewController(alert, animated: true, completion: nil)
+                    }
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
